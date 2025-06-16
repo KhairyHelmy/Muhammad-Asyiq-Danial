a6da9634fa31644c9402446f3077a9495d94f29f
ASYIQDANIAL <ASYIQDANIAL@172.17.179.106>
2025-06-17 02:27:59 +0800

    first commit broo

---
+++ src/main/java/java/com/util/DBConnection.java
@@ -0,0 +1,28 @@
+
+package com.util;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = System.getenv("DB_URL");
+    private static final String USER = System.getenv("DB_USER");
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.err.println("MySQL JDBC Driver not found.");
+            e.printStackTrace();
+        }
+
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
